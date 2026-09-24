fe8a9cc7c7b753eea848aa1d6903c5a3ab4d498d
Comic Trash <serikovastanislava@gmail.com>
2026-09-24 20:46:22 +0300

    fix: harden docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -eu
-
+echo "[backend] Applying database migrations..."
 python manage.py migrate --noinput
+echo "[backend] Collecting static files..."
 python manage.py collectstatic --noinput
-
+echo "[backend] Starting: $*"
 exec "$@"
